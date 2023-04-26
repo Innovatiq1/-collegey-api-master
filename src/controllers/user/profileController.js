1ea41a64cae8ba9c5c8dde54b68d6b9545a66b29
rasagna <rasagnapinninti@gmail.com>
2023-04-26 17:46:43 +0530

    projects are same

--- src/controllers/user/profileController.js
+++ src/controllers/user/profileController.js
@@ -2459,8 +2459,22 @@ exports.mentorDashboardNew = async function (req, res, next) {
 		
 		let getProjectsCollegytypeAggregate = [
 			{
-				$match: { projectType:'collegey' },
+				$match:
+				{
+					$and:
+						[
+							{ projectType :'collegey' },
+							{
+								"projectStatus":
+								{
+									$nin: ["new", "reject", "completed"]
+								}
+							},
+						],
+					status: 1
+				}
 			},
+			
 			{ $sort: { createdAt: -1 } },
 			{
 				$lookup:{
@@ -2470,7 +2484,9 @@ exports.mentorDashboardNew = async function (req, res, next) {
 					as: 'mentor'
 				}
 			},
-			{$unwind: '$mentor'},
+			{ $unwind: { path: "$mentor", preserveNullAndEmptyArrays: true } },
+
+
 			{
 				$lookup:{
 					from: 'users',
@@ -2479,7 +2495,7 @@ exports.mentorDashboardNew = async function (req, res, next) {
 					as: 'projectOwner'
 				}
 			},
-			{$unwind: '$projectOwner'},
+			{ $unwind: { path: "$projectOwner",  preserveNullAndEmptyArrays: true }  },
 			{
 				$lookup: {
 					from: 'users',
